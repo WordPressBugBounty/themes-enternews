a84975ea2f789766d1b552596a5dcae168972d75
Sean Murphy <sean@iamseanmurphy.com>
2026-02-06 06:36:59 +0000

    3.3.8.1

--- enternews/template-parts/content.php
+++ enternews/template-parts/content.php
@@ -31,6 +31,12 @@
                 </div>
             <?php endif; ?>
             <?php
+            // the_post_navigation(array(
+            //     'prev_text' => __('<span class="em-post-navigation">Previous</span> %title', 'enternews'),
+            //     'next_text' => __('<span class="em-post-navigation">Next</span> %title', 'enternews'),
+            //     //'in_same_term' => true,
+            //     'screen_reader_text' => __('Continue Reading', 'enternews'),
+            // ));
             the_post_navigation( array(
                 'prev_text' => sprintf(
                     /* translators: %s: Title of the previous post. */
@@ -47,6 +53,7 @@
                 /* translators: Hidden heading for the post navigation section. */
                 'screen_reader_text' => esc_html__( 'Post navigation', 'enternews' ),
             ) );
+            
             ?>
             <?php wp_link_pages(array(
                 'before' => '<div class="page-links">' . esc_html__('Pages:', 'enternews'),
